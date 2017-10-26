de9b85344aa04074c98a210142ee04de708d91c0
songliu <songliu@chinaums.com>
2017-10-26 14:09:04 +0800

    使用新的分页操作方法

--- cms/cms-web/src/main/java/com/zheng/cms/controller/UserController.java
+++ cms/cms-web/src/main/java/com/zheng/cms/controller/UserController.java
@@ -1,14 +1,9 @@
 package com.zheng.cms.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
+import com.zheng.cms.model.User;
+import com.zheng.cms.model.UserExample;
+import com.zheng.cms.service.UserService;
+import com.zheng.common.util.Paginator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +11,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.zheng.cms.model.User;
-import com.zheng.cms.model.UserExample;
-import com.zheng.cms.service.UserService;
-import com.zheng.cms.util.Paginator;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 用户控制器
@@ -37,9 +30,9 @@ import com.zheng.cms.util.Paginator;
 @Controller
 @RequestMapping("/user")
 public class UserController extends BaseController {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(UserController.class);
-	
+
 	@Autowired
 	private UserService userService;
 
@@ -47,7 +40,7 @@ public class UserController extends BaseController {
 	public void exceptionHandler(Exception e) {
 		e.printStackTrace();
 	}
-	
+
 	/**
 	 * 首页
 	 * @return
@@ -56,7 +49,7 @@ public class UserController extends BaseController {
 	public String index() {
 		return "redirect:/user/list";
 	}
-	
+
 	/**
 	 * 列表
 	 * @param page
@@ -68,19 +61,19 @@ public class UserController extends BaseController {
 	public String list(
 			@RequestParam(required = false, defaultValue = "1") int page,
 			@RequestParam(required = false, defaultValue = "20") int rows,
-			HttpServletRequest request) {
-		// 查询参数
-		String clumns = " * ";
-		String condition = " id>0 ";
-		String order = " id asc ";
-		Map<String,Object> parameters = new HashMap<String, Object>();
-		parameters.put("clumns", clumns);
-		parameters.put("condition", condition);
-		parameters.put("order", order);
-		// 创建分页对象
+			HttpServletRequest request, Model model) {
+
 		UserExample userExample = new UserExample();
 		userExample.createCriteria()
-			.andIdGreaterThan(0);
+				.andIdGreaterThan(0);
+		userExample.setOffset((page -1) * rows);
+		userExample.setLimit(rows);
+		userExample.setDistinct(false);
+		userExample.setOrderByClause(" id desc ");
+		List<User> users = userService.getMapper().selectByExample(userExample);
+		model.addAttribute("users", users);
+
+		// 创建分页对象
 		long total = userService.getMapper().countByExample(userExample);
 		Paginator paginator = new Paginator();
 		paginator.setTotal(total);
@@ -89,18 +82,11 @@ public class UserController extends BaseController {
 		paginator.setParam("page");
 		paginator.setUrl(request.getRequestURI());
 		paginator.setQuery(request.getQueryString());
-		// 调用有分页功能的方法
-		parameters.put("paginator", paginator);
-		List<User> users = userService.selectAll(parameters);
-		// 返回数据
-		request.setAttribute("users", users);
-		request.setAttribute("paginator", paginator);
-		
-		
-		//PageHelper.startPage(1, 10);
+		model.addAttribute("paginator", paginator);
+
 		return "/user/list";
 	}
-	
+
 	/**
 	 * 新增get
 	 * @return
@@ -109,7 +95,7 @@ public class UserController extends BaseController {
 	public String add() {
 		return "/user/add";
 	}
-	
+
 	/**
 	 * 新增post
 	 * @param user
@@ -128,7 +114,7 @@ public class UserController extends BaseController {
 		userService.getMapper().insertSelective(user);
 		return "redirect:/user/list";
 	}
-	
+
 	/**
 	 * 新增post2,返回自增主键值
 	 * @param user
@@ -145,7 +131,7 @@ public class UserController extends BaseController {
 		System.out.println(user.getId());
 		return "redirect:/user/list";
 	}
-	
+
 	/**
 	 * 删除
 	 * @param id
@@ -156,7 +142,7 @@ public class UserController extends BaseController {
 		userService.getMapper().deleteByPrimaryKey(id);
 		return "redirect:/user/list";
 	}
-	
+
 	/**
 	 * 修改get
 	 * @param id
@@ -168,7 +154,7 @@ public class UserController extends BaseController {
 		model.addAttribute("user", userService.getMapper().selectByPrimaryKey(id));
 		return "/user/update";
 	}
-	
+
 	/**
 	 * 修改post
 	 * @param id
@@ -186,13 +172,13 @@ public class UserController extends BaseController {
 		userService.getMapper().updateByPrimaryKeySelective(user);
 		return "redirect:/user/list";
 	}
-	
+
 	/**
 	 * 上传文件
 	 * @param file
 	 * @param request
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
@@ -201,11 +187,11 @@ public class UserController extends BaseController {
 		Map<String, Object> map = new HashMap<String, Object>();
 		// 判断上传文件类型
 		String contentType = file.getContentType().toLowerCase();
-		if ((!contentType.equals("image/jpeg")) && 
-				(!contentType.equals("image/pjpeg")) && 
-				(!contentType.equals("image/png")) && 
-				(!contentType.equals("image/x-png")) && 
-				(!contentType.equals("image/bmp")) && 
+		if ((!contentType.equals("image/jpeg")) &&
+				(!contentType.equals("image/pjpeg")) &&
+				(!contentType.equals("image/png")) &&
+				(!contentType.equals("image/x-png")) &&
+				(!contentType.equals("image/bmp")) &&
 				(!contentType.equals("image/gif"))) {
 			map.put(RESULT, FAILED);
 			map.put(DATA, "不支持该类型的文件！");
@@ -225,7 +211,7 @@ public class UserController extends BaseController {
 		map.put(DATA, targetFile.getAbsoluteFile());
 		return map;
 	}
-	
+
 	/**
 	 * ajax
 	 * @param id
@@ -236,5 +222,5 @@ public class UserController extends BaseController {
 	public Object ajax(@PathVariable int id) {
 		return userService.getMapper().selectByPrimaryKey(id);
 	}
-	
+
 }
