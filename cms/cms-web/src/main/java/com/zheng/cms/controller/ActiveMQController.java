3bb6261cda60e67ff1cb8ba0fce560af480129dd
songliu <songliu@chinaums.com>
2017-11-09 15:02:41 +0800

    update

--- cms/cms-web/src/main/java/com/zheng/cms/controller/ActiveMQController.java
+++ cms/cms-web/src/main/java/com/zheng/cms/controller/ActiveMQController.java
@@ -35,16 +35,17 @@ public class ActiveMQController extends BaseController {
 	public Object send() {
 		long start = System.currentTimeMillis();
 		User user = null;
-		for (int i = 0; i < 10000; i ++) {
+        for (int i = 1; i <= 1000; i ++) {
             user = new User();
-            user.setUsername("用户"+i);
+            user.setUsername("用户" + i);
             user.setPassword("123456");
             user.setNickname("昵称");
             user.setSex(1);
             user.setCtime(System.currentTimeMillis());
             user.setContent("用户描述");
-			JmsUtil.sendMessage(jmsQueueTemplate, defaultQueueDestination, JSONObject.fromObject(user).toString());
-		}
+//			jmsQueueTemplate.convertAndSend(defaultQueueDestination, user);
+            JmsUtil.sendMessage(jmsQueueTemplate, defaultQueueDestination, JSONObject.fromObject(user).toString());
+        }
 		_log.info("发送消息消耗时间" + (System.currentTimeMillis() - start));
 		return "success";
 	}
